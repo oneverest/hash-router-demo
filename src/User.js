f7ba2c0823cf1751cdcc40e9c2069a09146b60de
xieyu <4361605@qq.com>
2020-02-05 11:31:29 +0800

    fix

--- src/User.js
+++ src/User.js
@@ -9,21 +9,21 @@ export class User extends Component {
 
   render() {
     const {match} = this.props;
-    console.log('match', match);
+    // console.log('match', match);
 
     return ( 
       <div>
         <Grid columns={12} >
           <Grid.Column width={2}>
             <Menu vertical secondary>
-              <Menu.Item to="/user/list" as={Link}>用户列表</Menu.Item>
-              <Menu.Item to="/user/add" as={Link}>用户添加</Menu.Item>
+              <Menu.Item to={`${match.url}/list`} as={Link}>用户列表</Menu.Item>
+              <Menu.Item to={`${match.url}/add`} as={Link}>用户添加</Menu.Item>
             </Menu>
           </Grid.Column>
           <Grid.Column width={10}>
-            <Route path="/user/list" component={UserList} />
-            <Route path="/user/add" component={UserAdd} />
-            <Route path="/user/detail/:id" component={UserDetail} />
+            <Route path={`${match.url}/list`} component={UserList} />
+            <Route path={`${match.url}/add`} component={UserAdd} />
+            <Route path={`${match.url}/detail/:id`} component={UserDetail} />
           </Grid.Column>
         </Grid>
       </div>
